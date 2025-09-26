518d606718fe2ddc9f4b592bf51303df2e09a2bb
DaoChaShao <27882272@qq.com>
2025-09-26 13:00:11 +0800

    feat: add layout configuration and sidebar navigation for CNN app

--- utils/layout.py
+++ utils/layout.py
@@ -6,3 +6,61 @@
 # @File     :   layout.py
 # @Desc     :
 
+from streamlit import set_page_config, Page, navigation
+
+
+def page_config() -> None:
+    """ Set the window
+    :return: None
+    """
+    set_page_config(
+        page_title="CNN-VGG16 Cat & Dog Classifier",
+        page_icon=":material/globe:",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
+
+def pages_setter() -> None:
+    """ Set the subpages on the sidebar
+    :return: None
+    """
+    pages: dict = {
+        "page": [
+            "subpages/home.py",
+            "subpages/preparation.py",
+            "subpages/train.py",
+            "subpages/test.py",
+            "subpages/about.py",
+        ],
+        "title": [
+            "Home",
+            "Data Preparation",
+            "Model Training",
+            "Model Testing",
+            "About",
+        ],
+        "icon": [
+            ":material/home:",
+            ":material/dataset:",
+            ":material/function:",
+            ":material/assignment:",
+            ":material/info:",
+        ],
+    }
+
+    structure: dict = {
+        "Introduction": [
+            Page(page=pages["page"][0], title=pages["title"][0], icon=pages["icon"][0]),
+        ],
+        "Core Functions": [
+            Page(page=pages["page"][1], title=pages["title"][1], icon=pages["icon"][1]),
+            Page(page=pages["page"][2], title=pages["title"][2], icon=pages["icon"][2]),
+            Page(page=pages["page"][3], title=pages["title"][3], icon=pages["icon"][3]),
+        ],
+        "Information": [
+            Page(page=pages["page"][4], title=pages["title"][4], icon=pages["icon"][4]),
+        ],
+    }
+    pg = navigation(structure, position="sidebar", expanded=True)
+    pg.run()
