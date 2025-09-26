98234cc8ee6e74649b6ca135c671858b8bed469a
DaoChaShao <27882272@qq.com>
2025-09-26 17:04:12 +0800

    feat: add configuration file for data paths and model saving

---
+++ utils/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/26 14:58
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   config.py
+# @Desc     :   
+
+TRAIN_DATA_PATH = "data/train"
+TEST_DATA_PATH = "data/test"
+MODEL_SAVE_PATH = "vgg16_models.h5"
